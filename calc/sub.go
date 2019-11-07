d453b7ab0fa85937a6e2e4e3c1cc0bf28a20c120
duke <duke@itcast.cn>
2019-11-07 10:48:09 +0800

    添加sub函数

---
+++ calc/sub.go
@@ -0,0 +1,5 @@
+package calc
+
+func Sub(a, b int) int {
+	return a - b
+}
--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
+	fmt.Println("calc.Add called!")
 	res := calc.Add(10, 20)
-
 	fmt.Println("Add(10 ,20) :", res)
+
+	fmt.Println("calc.Sub called!")
+	res = calc.Sub(30, 20)
+	fmt.Println("Sub(10 ,20) :", res)
+	//fmt
 }
